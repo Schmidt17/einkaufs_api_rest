e65bd087e3b2ecab60299e30447b95635228e17e
ah <andreas.hofacker@posteo.de>
2024-08-14 14:51:29 +0200

    Add postgres engine

--- einkaufs_api_rest/settings.py
+++ einkaufs_api_rest/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'einkaufs_api_rest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'einkaufs_api',
+        'USER': credentials.postgres_db_user,
+        'PASSWORD': credentials.postgres_db_password,
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
