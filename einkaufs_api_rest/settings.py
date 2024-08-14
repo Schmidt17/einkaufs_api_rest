fc26b4a80bf3f09b592bd476e8f68cd6d7d60ca0
ah <andreas.hofacker@posteo.de>
2024-08-14 23:50:08 +0200

    Update allowed hosts

--- einkaufs_api_rest/settings.py
+++ einkaufs_api_rest/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-_&dyn3@sz57nxp@v%=j%^mg!62qo1(_@p3myzwa6%a_og#2%^+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', 'node08']
 
 
 # Application definition
