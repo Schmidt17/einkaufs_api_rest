6bf24749400a635aa5a103b3df483eed0968a2ae
ah <andreas.hofacker@posteo.de>
2024-08-15 15:34:35 +0200

    Move secret key to env var

--- einkaufs_api_rest/settings.py
+++ einkaufs_api_rest/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_&dyn3@sz57nxp@v%=j%^mg!62qo1(_@p3myzwa6%a_og#2%^+'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DJANGO_DEBUG')))
 
-#ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', 'node08']
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(" ")
 
 # Application definition
