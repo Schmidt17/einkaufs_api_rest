9fa7d7b349353164f2ddfb3f5af8204a2deab7e4
ah <andreas.hofacker@posteo.de>
2024-08-15 23:42:19 +0200

    Change name and item_id from CharField to TextField

--- collect/models.py
+++ collect/models.py
@@ -7,8 +7,8 @@ class CollectedItem(models.Model):
         UNCROSSED = 'UNCROSSED'
 
     action_type = models.CharField(max_length=255, choices=ActionType.choices)
-    name = models.CharField(max_length=255)
-    item_id = models.CharField(max_length=32)
+    name = models.TextField()
+    item_id = models.TextField()
     latitude = models.FloatField(null=True, blank=True, default=None)
     longitude = models.FloatField(null=True, blank=True, default=None)
     timestamp = models.DateTimeField(auto_now_add=True)
