9b631cf8bb12e92b321eaf4fcdb31394c54fd34f
ah <andreas.hofacker@posteo.de>
2024-10-29 16:02:38 +0100

    Change package name to tflite-runtime, define new loading function

--- collect/views.py
+++ collect/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 
 from pathlib import Path
 import cloudpickle
+from tflite_runtime import interpreter as tflite
+from functools import partial
 
 from .models import CollectedItem
 from .serializers import CollectedItemSerializer
@@ -19,9 +21,26 @@ class CollectItem(generics.CreateAPIView):
     serializer_class = CollectedItemSerializer
 
 
-def load_model(path):
+class TFLiteEstimator:
+    def __init__(self, tflite_model):
+        self.callable = tflite_model.get_signature_runner()
+        self.signature_list = tflite_model.get_signature_list()
+
+        self.input_tensor_name = self.signature_list['serving_default']['inputs'][0]
+        self.output_tensor_name = self.signature_list['serving_default']['outputs'][0]
+
+
+    def predict(self, input_data):
+        return self.callable(**{self.input_tensor_name: input_data})[self.output_tensor_name]
+
+
+def load_model(path, tflite_path=None):
     with open(path, 'rb') as f:
         model = cloudpickle.load(f)
+
+    if tflite_path is not None:
+        tflite_model = tflite.Interpreter(str(tflite_path))
+        model = partial(model, model=TFLiteEstimator(tflite_model))
 
     return model
 
